d4224d0492c015b00a58e4ff45f29b7cc228665f
Dan <dantheeman763@gmail.com>
2019-06-23 16:59:57 -0500

    fixed root file directory issue

--- server/server.js
+++ server/server.js
@@ -28,9 +28,9 @@ app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('../client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build/index.html'));
+        res.sendFile('../client/build/index.html');
     })
 }
 
